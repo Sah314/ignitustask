d8e6e665745808d8cd6857e0a34d4533a939b9c0
sah_314 <khadayatesahil@gmail.com>
2023-10-06 21:48:52 +0530

    added cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {Itemspurchased} from '../features/CartSlice';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const cartitems = useSelector((state)=>state.cart)
+  const handleBuyitems=()=>{
+    
+    dispatch(Itemspurchased(cartitems))
+    console.log(cartitems);
+       // console.log("handleBuy")
+  }
+
+  return (
+    <div>
+      <button onClick={handleBuyitems}>Buy items</button>
+      <Link to="/">Go to Homepage</Link>
+    </div>
+  )
+}
+
+export default Cart
