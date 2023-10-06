090f5a00de1398107ab7cd70510e19cff71a3993
sah_314 <khadayatesahil@gmail.com>
2023-10-06 21:08:39 +0530

    Added reducers to Cartslice

--- src/features/Cartslice.js
+++ src/features/Cartslice.js
@@ -1,22 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-    
-]
+const initialState = {
+    cart:[
 
-const CartSlice = createSlice({
-  name: 'todos',
+    ],
+    items:[
+        {
+            imgpath:"img1.png",
+            cost:50
+          },
+          {
+            imgpath:"img2.png",
+            cost:50
+          },
+          {
+            imgpath:"img3.png",
+            cost:50
+          }
+    ]
+}
+
+export const CartSlice = createSlice({
+  name: 'cart',
   initialState,
   reducers: {
     
     ItemAdded(state,action){
-
+        const {imgpath,cost} = action.payload;
+        let present = state.cart.find((ele)=>{
+            return ele.imgpath===imgpath
+        })
+        if(!present){
+            state.cart.push({
+                imgpath:imgpath,
+                cost:cost
+            })
+        }
+        else{
+            console.log("element already present");
+        }
     }
-    
+    ,
+    Itemspurchased(state,action){
+         state.items = state.items.filter(ele=>{
+            return state.cart.some(ele2=>
+                ele.imgpath===ele2.imgpath && ele.cost===ele2.cost)
+         })
+         
+    }
   }
 })
 
 
-export const { todoAdded, todoToggled } = CartSlice.actions
+export const { ItemAdded, Itemspurchased } = CartSlice.actions
 
 export default CartSlice.reducer
