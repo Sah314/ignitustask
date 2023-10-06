1714581984bc8d213bb377c4e1079a9a6bd7fb8b
sah_314 <khadayatesahil@gmail.com>
2023-10-06 21:10:56 +0530

    Added buying logic to cart elements

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,51 @@
 import React from 'react'
 import './Cards.css'
-const Cards = ({pictures}) => {
+import { useState,useEffect } from 'react';
+import {ItemAdded, Itemspurchased} from '../features/CartSlice';
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+
+const Cards = () => {
+  //const selector = new useSelector();
+  const [isEmpty,setIsEmpty] = useState(false);
+  const pictures = useSelector((state)=>state.items)
+ // console.log(pictures);
+  const dispatch = useDispatch();
+
+  const handleBuy=(obj)=>{
+
+    dispatch(ItemAdded(obj));
+    alert("successfully added to cart")
+console.log(obj);
+
+  }
+
+  useEffect(() => {
+   if(pictures.length===0){
+
+   }
+  }, [pictures])
+  
   return (
     <>
     <div className='cardcont'>
-{        pictures.map((obj)=>(<div className='card' key={obj.imgpath}>
+
+{  !isEmpty  &&  (pictures.map((obj)=>(<div className='card' key={obj.imgpath}>
   <img src={obj.imgpath} alt="" className='image'/>
   <div className='buyitem'>
 <div style={{marginLeft:"3rem"}}>{obj.cost}ETH</div>
-<button type="submit" style={{marginRight:"3rem"}}>Buy</button>
+<button style={{marginRight:"3rem"}} onClick={()=>handleBuy(obj)}>Buy</button>
   </div>
 </div>)
+))
+}
+{
+  isEmpty&& (
+    <div>No items to display</div>
+  )
+}
 
-)
-}    </div>
+    </div>
     </>
   )
 }
