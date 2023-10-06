097a481aeb0ef67fc6a1e89f4bac27e5ea097b5e
sah_314 <khadayatesahil@gmail.com>
2023-10-06 21:52:52 +0530

    added proper routing to application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Cards from './components/Cards';
+import Cart from './components/Cart';
+import {BrowserRouter as Router,Routes,Route, Link} from 'react-router-dom'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <Link to="/cart">To Cart</Link>
+        <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/cart" element={<Cart/>} />
+        </Routes>
     </div>
+    </Router>
+    
   );
 }
-
 export default App;
